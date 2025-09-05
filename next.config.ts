145679b175672b14dbf1ad4ccec1e05cc97dea55
poieticid <ypsiron@gmail.com>
2025-09-05 17:39:15 +0900

    commit fragmentpractice.com

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
+// next.config.js
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
+
+  async headers() {
+    return [
+      {
+        source: "/story",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex, nofollow",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
